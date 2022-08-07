7e1f681d97a4493d16a8239138b02be3e5a7ef0e
Shaun <shaun@thenetninja.co.uk>
2022-08-07 18:56:02 +0100

    lesson-5

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom'
+
 const SmoothieCard = ({ smoothie }) => {
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+      </div>
     </div>
   )
 }
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,7 +1,64 @@
+import { useEffect, useState } from "react"
+import { useParams, useNavigate } from 'react-router-dom'
+import supabase from "../config/supabaseClient"
+
 const Update = () => {
-  return (
-    <div className="page update">
-      <h2>Update</h2>
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState('')
+
+  useEffect(() => {
+    const fetchSmoothie = async () => {
+      const { data, error } = await supabase
+        .from('recipes')
+        .select()
+        .eq('id', id)
+        .single()
+
+      if (error) {
+        navigate('/', { replace: true })
+      }
+      if (data) {
+        setTitle(data.title)
+        setMethod(data.method)
+        setRating(data.rating)
+      }
+    }
+
+    fetchSmoothie()
+  }, [id, navigate])
+
+  return title && (
+    <div className="page create">
+      <form>
+        <label htmlFor="title">Title:</label>
+        <input 
+          type="text" 
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea 
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input 
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Update Smoothie Recipe</button>
+      </form>
     </div>
   )
 }

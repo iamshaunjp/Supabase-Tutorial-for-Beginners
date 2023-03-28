f3f4cdb3fbb72540823c7676172c2422533fe483
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-03-28 17:17:33 +0200

    create new branch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
+import supabase from "../config/supabaseClints";
+
 const Home = () => {
+  console.log(supabase);
+
   return (
-    <div className="page home">
+    <div className='page home'>
       <h2>Home</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
